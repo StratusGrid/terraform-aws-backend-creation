8711cbec2af9f1feccc6002896184fd03b055c43
JoseMotto-sg <jose.motto@stratusgrid.com>
2022-09-27 13:23:19 +0200

    delete brew dependencies

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
 
 resource "aws_s3_bucket_public_access_block" "default" {
   bucket                  = aws_s3_bucket.this.id
-  block_public_acls       = false
+  block_public_acls       = true
   ignore_public_acls      = true
   block_public_policy     = true
   restrict_public_buckets = true
